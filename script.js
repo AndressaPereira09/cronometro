ae2ca6254b85db7ee6212aae9ae49d334b9237c5
Andressa Pereira <115735033+AndressaPereira09@users.noreply.github.com>
2023-03-25 14:07:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+window.onload = function () {
+    //Executa um JS imediatamente após o carregamento da página.
+
+    var seconds = 00;
+    var tens = 00;
+
+    var appendTens = document.getElementById("tens");
+    var appendSeconds = document.getElementById("seconds");
+
+    var buttonStart = document.getElementById("button-start");
+    var buttonStop = document.getElementById("button-stop");
+    var buttonReset = document.getElementById("button-reset");
+
+    var interval;
+
+    buttonStart.onclick = function () {
+        clearInterval(interval); //O método ClearInterval() limpa o temporizador definido com o método setInterval().
+
+        interval = setInterval(startTimer, 10);
+        //O método setInterval() chama uma função em intervalos específicos (milissegundos).
+    };
+
+    buttonStop.onclick = function () {
+        clearInterval(interval);
+    };
+
+    buttonReset.onclick = function () {
+        clearInterval(interval);
+        tens = "00";
+        seconds = "00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    };
+
+    function startTimer() {
+        tens++ //tens++ pôs incremento --- ++tens pré incremento.
+        if (tens <= 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+
+        if (tens > 9) {
+            appendTens.innerHTML = tens;
+        }
+
+        if (tens > 99) {
+            console.log("seconds");
+
+            seconds++
+
+            appendSeconds.innerHTML = "0" + seconds
+
+            tens = 0;
+
+            appendTens.innerHTML = "0" + 0
+        }
+
+        if (seconds > 9) {
+            appendSeconds.innerHTML = seconds
+        }
+    }
+
+}
